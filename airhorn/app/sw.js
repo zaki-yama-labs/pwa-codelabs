6fba37c0c53e8c6a8dfca232ac1bebf5ae933031
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-03-21 23:22:59 +0900

    Intercept the web page requests

--- airhorn/app/sw.js
+++ airhorn/app/sw.js
@@ -15,3 +15,13 @@ self.addEventListener('install', function(e) {
    })
  );
 });
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
