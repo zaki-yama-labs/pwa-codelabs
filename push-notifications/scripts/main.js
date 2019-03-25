715273c3e443532dc6416a71479d813b10a6738b
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-03-25 21:33:15 +0900

    #3 10. Unsubscribe the user

--- push-notifications/scripts/main.js
+++ push-notifications/scripts/main.js
@@ -48,7 +48,7 @@ function initializeUI() {
   pushButton.addEventListener('click', function() {
     pushButton.disabled = true;
     if (isSubscribed) {
-      // TODO: Unsubscribe user
+      unsubscribeUser();
     } else {
       subscribeUser();
     }
@@ -86,6 +86,26 @@ function subscribeUser() {
   })
   .catch(function(err) {
     console.log('Failed to subscribe the user: ', err);
+
+    updateBtn();
+  });
+}
+
+function unsubscribeUser() {
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    if (subscription) {
+      return subscription.unsubscribe();
+    }
+  })
+  .catch(function(error) {
+    console.log('Error unsubscribing', error);
+  })
+  .then(function() {
+    updateSubscriptionOnServer(null);
+
+    console.log('User is unsubscribed.');
+    isSubscribed = false;
 
     updateBtn();
   });
