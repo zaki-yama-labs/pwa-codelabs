2459f178de334b1ac4785633df933313c6e2ac71
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2019-03-25 20:07:36 +0900

    #3 4. Initialize State

--- push-notifications/scripts/main.js
+++ push-notifications/scripts/main.js
@@ -44,6 +44,32 @@ function urlB64ToUint8Array(base64String) {
   return outputArray;
 }
 
+function initializeUI() {
+  // Set the initial subscription value
+  swRegistration.pushManager.getSubscription()
+  .then(function(subscription) {
+    isSubscribed = !(subscription === null);
+
+    if (isSubscribed) {
+      console.log('User IS subscribed');
+    } else {
+      console.log('User is NOT subscribed');
+    }
+
+    updateBtn();
+  });
+}
+
+function updateBtn() {
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
 if ('serviceWorker' in navigator && 'PushManager' in window) {
   console.log('Service Worker and Push is supported');
 
@@ -52,6 +78,7 @@ if ('serviceWorker' in navigator && 'PushManager' in window) {
     console.log('Service Worker is registered', swReg);
 
     swRegistration = swReg;
+    initializeUI();
   })
   .catch(function(error) {
     console.error('Service Worker Error', error);
